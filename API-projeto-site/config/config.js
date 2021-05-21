11c1ab6799eb6173a3f4b3cb8ad8123e006a4b5d
joaopedpaula <joao.paula@bandtec.com.br>
2021-05-21 12:52:37 -0300

    Set database config

--- API-projeto-site/config/config.js
+++ API-projeto-site/config/config.js
@@ -25,9 +25,9 @@ module.exports = {
   // Insira aqui seus dados do banco LOCAL - MySQL Workbench
   dev: {
     // altere APENAS username, password e database.
-    username: 'root',
-    password: '#Dravhelios',
-    database: 'projeto_iot',
+    username: 'SEU_USUARIO_DO_BANCO_DE_DADOS',
+    password: 'SUA_SENHA_DO_BANCO_DE_DADOS',
+    database: 'NOME_DO_SEU_DATABASE',
     host: 'localhost',
     dialect: 'mysql',
     xuse_env_variable: 'DATABASE_URL',
